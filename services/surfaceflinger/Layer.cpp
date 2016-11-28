fb032ff0b4d445f512836241d664afb2ce58d85a
Arun Kumar K.R <akumarkr@codeaurora.org>
2016-11-27 21:09:47 -0800

    SurfaceFlinger: Avoid HDR layers going to GPU
    The HDR layers when fall back to GPU, show differently due to missing
    tonemapping in fall back path, hence avoid those layers going to GPU.
    
    CRs-Fixed: 1094808
    Change-Id: I68072f9a04042fd011c53ecddf9ca22c1507fa12

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -35,6 +35,12 @@
 #include <utils/Trace.h>
 
 #include <ui/DebugUtils.h>
+
+#ifdef QTI_BSP
+#include <gralloc_priv.h>
+#include <qdMetaData.h>
+#endif
+
 #include <ui/GraphicBuffer.h>
 #include <ui/PixelFormat.h>
 
@@ -1077,8 +1083,7 @@ void Layer::onDraw(const sp<const DisplayDevice>& hw, const Region& clip,
         // Go ahead and draw the buffer anyway; no matter what we do the screen
         // is probably going to have something visibly wrong.
     }
-
-    bool blackOutLayer = isProtected() || (isSecure() && !hw->isSecure());
+    bool blackOutLayer = isProtected() || (isSecure() && !hw->isSecure()) || isHDRLayer();
 
     RenderEngine& engine(mFlinger->getRenderEngine());
 
