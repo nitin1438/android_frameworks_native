8da3c2672c332e8900b48b31827f026d537eb2ed
Chris <chris@cypheros.co>
2017-11-30 22:28:42 -0500

    SurfaceFlinger: disable some logging to fix compilation
    - Usage refers to unsigned_int but is defined as
    uint64_t, fix it later
    
    Change-Id: Id3babfd2fec9afe2a74181067c32a3ff150b17aa

--- services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
+++ services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
@@ -348,8 +348,9 @@ status_t VirtualDisplaySurface::dequeueBuffer(Source source,
     // Exclude video encoder usage flag from scratch buffer usage flags.
     if (source == SOURCE_SCRATCH) {
         usage &= ~(GRALLOC_USAGE_HW_VIDEO_ENCODER);
-        VDS_LOGV("dequeueBuffer(%s): updated scratch buffer usage flags=%#x",
-                dbgSourceStr(source), usage);
+		//Disabled: Temporary compilation fix
+        //VDS_LOGV("dequeueBuffer(%s): updated scratch buffer usage flags=%#x",
+                //dbgSourceStr(source), usage);
     }
 
     status_t result = mSource[source]->dequeueBuffer(sslot, fence,
